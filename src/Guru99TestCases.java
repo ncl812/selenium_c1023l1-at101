67c66ae68294268103cfaff750f762f2f7c7af7a
ncl812 <nguyencaoly812@gmail.com>
2023-12-26 19:33:43 +0700

    0

---
+++ src/Guru99TestCases.java
@@ -0,0 +1,18 @@
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Guru99TestCases {
+
+    WebDriver driver;
+
+    public void beforeClass() {
+        System.setProperty("webdriver.chrome.driver", ".\\driver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://demoqa.com/automation-practice-form");
+    }
+}
